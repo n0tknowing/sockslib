77c486c628c18e7fd3fec47d43bb3eb7c6af8d28
Nathanael Eka Oktavian <nathanael@nand.eu.org>
2022-05-08 11:23:07 +0700

    test: simplify test

--- test.c
+++ test.c
@@ -70,23 +70,8 @@ int main(int argc, char **argv)
 	if (ret < 0)
 		goto fail;
 
-	/* if success, let's GET a HTTP web */
-	char buf[10];
-	ssize_t r, k = 1;
-
-	dprintf(ret, "GET / HTTP/1.1\r\n"
-		    "Host: %s:%s\r\n"
-		    "\r\n", host, port);
-
-	while (k) {
-		r = recv(ret, buf, 9, 0);
-		if (r != 9)
-			k = 0;
-		buf[r] = 0;
-		printf("%s", buf);
-	}
-
-	printf("\n");
+	/* success */
+	printf("Connected, file descriptor = %d\n", ret);
 
 fail:
 	/* Example use of socks_strerror() and errno handling */
