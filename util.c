73e52d6b21648c4e2f933c0c5362c0d1d862a1ed
Nathanael Eka Oktavian <nathanael@nand.eu.org>
2022-05-08 20:28:02 +0700

    non-blocking

--- util.c
+++ util.c
@@ -1,7 +1,10 @@
 #define _POSIX_C_SOURCE 200809L
 #include <errno.h>
+#include <fcntl.h>
+#include <netinet/tcp.h>
 #include <stddef.h>
 #include <sys/types.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -10,38 +13,101 @@
 
 int sockslib_send(int fd, const void *buf, size_t len)
 {
-	ssize_t ret = 0;
-	const unsigned char *s = buf;
+	ssize_t ret;
+	const unsigned char *send_buf = buf;
 
-	while (len) {
-		ret = send(fd, s, len, 0);
-		if (ret > 0) {
-			len -= ret;
-			s += ret;
-			continue;
-		} else if (ret < 0) {
+	for (;;) {
+		ret = send(fd, send_buf, len, 0);
+		if (ret >= 0)
+			break;
+
+		if (errno != EAGAIN && errno != EWOULDBLOCK)
 			return -SOCKS_ERR_SYS_ERRNO;
-		} else {
-			return -SOCKS_ERR_EMPTY_RESP;
-		}
 	}
 
-	return SOCKS_ERR_OK;
+	return ret ? SOCKS_ERR_OK : -SOCKS_ERR_EMPTY_RESP;
 }
 
 int sockslib_read(int fd, void *buf, size_t len)
 {
 	ssize_t ret;
 
-	ret = recv(fd, buf, len, 0);
-	if (ret < 0)
-		return -SOCKS_ERR_SYS_ERRNO;
+	for (;;) {
+		ret = recv(fd, buf, len, 0);
+		if (ret >= 0)
+			break;
+
+		if (errno != EAGAIN && errno != EWOULDBLOCK)
+			return -SOCKS_ERR_SYS_ERRNO;
+	}
 
 	return ret ? SOCKS_ERR_OK : -SOCKS_ERR_EMPTY_RESP;
 }
 
-int sockslib_connect(int fd)
+int sockslib_connect(int fd, const struct sockaddr *addr, socklen_t len)
 {
-	(void)fd;
+	int ret;
+	fd_set set;
+	struct timeval tm;
+
+	tm.tv_sec = 15;
+	tm.tv_usec = 0;
+
+	ret = connect(fd, addr, len);
+	if (ret < 0) {
+		if (errno != EINPROGRESS) {
+			ret = -SOCKS_ERR_SYS_ERRNO;
+			goto done;
+		}
+
+		for (;;) {
+			FD_ZERO(&set);
+			FD_SET(fd, &set);
+
+			ret = select(fd + 1, NULL, &set, NULL, &tm);
+			if (ret < 0 && errno != EINTR) {
+				ret = -SOCKS_ERR_SYS_ERRNO;
+				goto done;
+			} else if (FD_ISSET(fd, &set)) {
+				ret = SOCKS_ERR_OK;
+				goto done;
+			} else {
+				ret = -SOCKS_ERR_CONN_TIMEOUT;
+				goto done;
+			}
+		}
+	}
+
+done:
+	return ret;
+}
+
+int sockslib_set_nonblock(int fd, int opt)
+{
+	int ret;
+
+	ret = fcntl(fd, F_GETFL, NULL);
+	if (ret < 0)
+		return -SOCKS_ERR_SYS_ERRNO;
+
+	if (opt)
+		ret |= O_NONBLOCK;
+	else
+		ret &= (~O_NONBLOCK);
+
+	if (fcntl(fd, F_SETFL, ret) < 0)
+		return -SOCKS_ERR_SYS_ERRNO;
+
+	return SOCKS_ERR_OK;
+}
+
+int sockslib_set_nodelay(int fd, int opt)
+{
+	int ret;
+
+	ret = setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(int));
+	if (ret < 0)
+		return -SOCKS_ERR_SYS_ERRNO;
+
 	return SOCKS_ERR_OK;
 }
--- util.h
+++ util.h
@@ -2,9 +2,12 @@
 #define SOCKSLIB_UTIL_H
 
 #include <stddef.h>
+#include <sys/types.h>
 
 int sockslib_send(int, const void *, size_t);
 int sockslib_read(int, void *, size_t);
-int sockslib_connect(int);
+int sockslib_connect(int, const struct sockaddr *, socklen_t);
+int sockslib_set_nonblock(int, int);
+int sockslib_set_nodelay(int, int);
 
 #endif /* SOCKSLIB_UTIL_H */
